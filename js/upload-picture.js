c26ecab0f06224d1c619a40e6400164f73bcd509
skudinva <skudinva@gmail.com>
2024-02-26 11:44:33 +0700

    Update upload-picture.js
    Добавил валидацию pristine

--- js/upload-picture.js
+++ js/upload-picture.js
@@ -1,13 +1,41 @@
-export const initUploadPicture = function() {
+import { Pristine } from '../vendor/pristine/pristine.min';
+import { validateHashtag, validateStringLen } from './utils';
+
+export const initUploadPicture = function () {
   const uploadPictureForm = document.querySelector('.img-upload__form');
-  const uploadPictureInput = uploadPictureForm.querySelector('.img-upload__input');
-  const uploadPictureOverlay = uploadPictureForm.querySelector('.img-upload__overlay');
-  const uploadPictureFormCancel = uploadPictureForm.querySelector('.img-upload__cancel');
-  const uploadPicturePreview = uploadPictureOverlay.querySelector('.img-upload__preview');
+  const uploadPictureInput =
+    uploadPictureForm.querySelector('.img-upload__input');
+  const uploadPictureOverlay = uploadPictureForm.querySelector(
+    '.img-upload__overlay'
+  );
+  const uploadPictureFormCancel = uploadPictureForm.querySelector(
+    '.img-upload__cancel'
+  );
+  const uploadPicturePreview = uploadPictureOverlay.querySelector(
+    '.img-upload__preview'
+  );
   const uploadPicturePreviewImg = uploadPicturePreview.querySelector('img');
+  const pristine = new Pristine(uploadPictureForm, {}, false);
 
-  uploadPictureForm.addEventListener('submit', () => {
+  pristine.addValidator(
+    uploadPictureForm.querySelector('.text__hashtags'),
+    validateHashtag,
+    'Начинается с #, до 19 символов и цифр'
+  );
 
+  const validateDescription = (description) => {
+    validateStringLen(description, 140);
+  };
+
+  pristine.addValidator(
+    uploadPictureForm.querySelector('.text__description'),
+    validateDescription,
+    'не более 140 символов'
+  );
+
+  uploadPictureForm.addEventListener('submit', (evt) => {
+    evt.preventDefault();
+    pristine.validate();
   });
 
   uploadPictureInput.addEventListener('change', () => {
@@ -16,9 +44,11 @@ export const initUploadPicture = function() {
     uploadPicturePreviewImg.src = uploadPictureInput.value;
   });
 
-  const onClickClose = function(evt) {
-    if (evt.type === 'keydown' && evt.key === 'Escape'
-    || evt.type === 'click') {
+  const onClickClose = function (evt) {
+    if (
+      (evt.type === 'keydown' && evt.key === 'Escape') ||
+      evt.type === 'click'
+    ) {
       uploadPictureOverlay.classList.add('hidden');
       document.body.classList.remove('modal-open');
       uploadPictureInput.value = '';

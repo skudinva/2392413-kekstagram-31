81a36d286e177c07fb65c719698a09a671bb871b
Skudin Vladimir <skudinva@gmail.com>
2024-03-07 20:02:46 +0700

    Добавил функции: - получения уникальных элементов из массива; - устранения дребезга.

--- js/utils.js
+++ js/utils.js
@@ -8,6 +8,18 @@ const getRandomInteger = (a, b) => {
 const getRandomArrayElement = (elements) =>
   elements[getRandomInteger(0, elements.length - 1)];
 
+const getUniqueRandomArrayElement = (elements, maxCount) => {
+  const uniqueueIndex = new Set();
+  while (uniqueueIndex.size !== Math.min(maxCount, elements.length)) {
+    uniqueueIndex.add(getRandomInteger(0, elements.length - 1));
+  }
+
+  // return elements.filter((element, index) => uniqueueIndex.has(index));
+  const result = [];
+  uniqueueIndex.forEach((value) => result.push(elements[value]));
+  return result;
+};
+
 const validateHashtag = (hashtag) => /^#[a-zа-яё0-9]{1,19}$/i.test(hashtag);
 
 const allowHashtagChar = (char) => /[#a-zа-яё0-9 ]/.test(char);
@@ -16,10 +28,20 @@ const validateStringLen = (str, len) => str.length <= len;
 
 const strLenCheck = (str, len) => str.length <= len;
 
+const debounce = (callback, timeoutDelay) => {
+  let timeoutId;
+  return (...rest) => {
+    clearTimeout(timeoutId);
+    timeoutId = setTimeout(() => callback.apply(this, rest), timeoutDelay);
+  };
+};
+
 export {
   allowHashtagChar,
+  debounce,
   getRandomArrayElement,
   getRandomInteger,
+  getUniqueRandomArrayElement,
   strLenCheck,
   validateHashtag,
   validateStringLen,

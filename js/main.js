1be6d3a7ec02737223d534d11771404b9505da05
Skudin Vladimir <skudinva@gmail.com>
2024-03-12 20:23:19 +0700

    Убрал лишний вызов отрисовки

--- js/main.js
+++ js/main.js
@@ -1,6 +1,5 @@
 import { showAlert } from './alert';
 import { getData } from './api';
-import { createThumbnails } from './draw-thumbnails';
 import { initFilters } from './filter-picture';
 import { setPictures } from './picture-state';
 import { initUploadPicture } from './upload-picture';
@@ -9,7 +8,6 @@ getData()
   .then((pictures) => {
     setPictures(pictures);
     initFilters();
-    createThumbnails();
   })
   .catch((err) => {
     showAlert(err.message);

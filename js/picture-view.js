22ae1bf5345745f78801089c255e3292760e816f
Skudin Vladimir <skudinva@gmail.com>
2024-03-14 16:39:52 +0700

    Доделал форму просмотра фото

--- js/picture-view.js
+++ js/picture-view.js
@@ -1,58 +1,15 @@
-import { COMMENT_LOADING_COUNT } from './config';
 import {
   bigPicture,
   bigPictureCancel,
   bigPictureImgTag,
-  commentCount,
   commentLoader,
-  commentsShowCount,
-  commentsTotalCount,
   likesCount,
   pictureComments,
   pictureDescription,
 } from './page-elements';
-import {
-  getComments,
-  getLastCommentShowItem,
-  getSelectedPicture,
-  resetSelectedPicture,
-  setLastCommentShowItem,
-} from './picture-state';
-import { addOrRemoveClass, isEscapeKey } from './utils';
-
-/**
- * Создание иконци аватара в списке с комментариями
- */
-const createAvatar = function (avatar, name) {
-  const userAvatar = document.createElement('img');
-  userAvatar.classList.add('social__picture');
-  userAvatar.src = avatar;
-  userAvatar.alt = name;
-  userAvatar.width = 35;
-  userAvatar.height = 35;
-  return userAvatar;
-};
-
-/**
- * Создание текста коммментария
- */
-const createMessage = function (message) {
-  const userMessage = document.createElement('p');
-  userMessage.classList.add('social__text');
-  userMessage.textContent = message;
-  return userMessage;
-};
-
-/**
- * Создание комментария
- */
-const createComment = function ({ avatar, name, message }) {
-  const userComment = document.createElement('li');
-  userComment.classList.add('social__comment');
-  userComment.appendChild(createAvatar(avatar, name));
-  userComment.appendChild(createMessage(message));
-  return userComment;
-};
+import { initCommentBlock, onCommentLoaderClick } from './picture-comments';
+import { getSelectedPicture, resetSelectedPicture } from './picture-state';
+import { isEscapeKey } from './utils';
 
 /**
  * Обработчик события клик на закрытие фото (крестик в правом углу)
@@ -73,33 +30,18 @@ const onPictureCloseKeydown = function (evt) {
 };
 
 /**
- * Обработчиск собития клик на ссылку "Загрузить еще".
- * Подгрузка комментариев. Количество комментариев для подгрузки в
- * константе COMMENT_LOADING_COUNT.
- * Если все комментарии подгружены, то скрываем ссылку.
+ * Функция добавляет фрагмент с пачкой комментариев на форму
  */
-const onCommentLoaderClick = function () {
-  const comments = getComments();
-
-  const lastCommentShowItem = Math.max(getLastCommentShowItem(), 0);
-  const newLastCommentShowItem = Math.min(
-    lastCommentShowItem + COMMENT_LOADING_COUNT,
-    comments.length
-  );
-
-  comments
-    .slice(lastCommentShowItem, newLastCommentShowItem)
-    .forEach((comment) => {
-      pictureComments.appendChild(createComment(comment));
-    });
-
-  commentsShowCount.textContent = newLastCommentShowItem;
-  setLastCommentShowItem(newLastCommentShowItem);
-
-  if (newLastCommentShowItem >= comments.length) {
-    commentLoader.classList.add('hidden');
-  }
+const appendNewComments = function (commentFragment) {
+  pictureComments.appendChild(commentFragment);
 };
+
+/**
+ * Добавляем событие клик на ссылку подгрузки комментариев
+ */
+commentLoader.addEventListener('click', (evt) => {
+  onCommentLoaderClick(evt.target, appendNewComments);
+});
 
 /**
  * Закрытие формы просмотра фото.
@@ -108,22 +50,8 @@ function formClose() {
   bigPicture.classList.add('hidden');
   document.body.classList.remove('modal-open');
   document.removeEventListener('keydown', onPictureCloseKeydown);
-  commentLoader.removeEventListener('click', onCommentLoaderClick);
   resetSelectedPicture();
 }
-
-/**
- * Инициализация блока с комментариями.
- */
-const initCommentBlock = function () {
-  const comments = getComments();
-  commentsTotalCount.textContent = comments.length;
-  commentCount.classList.remove('hidden');
-  addOrRemoveClass(commentLoader, 'hidden', comments.length === 0);
-  pictureComments.replaceChildren();
-  commentLoader.addEventListener('click', onCommentLoaderClick);
-  onCommentLoaderClick();
-};
 
 /**
  * Отрисовка фото
@@ -133,7 +61,9 @@ const renderBigPicture = function () {
   bigPictureImgTag.src = url;
   likesCount.textContent = likes;
   pictureDescription.textContent = description;
+  pictureComments.replaceChildren();
   initCommentBlock();
+  onCommentLoaderClick(commentLoader, appendNewComments);
   bigPicture.classList.remove('hidden');
   bigPictureCancel.addEventListener('click', onPictureCloseClick);
   document.addEventListener('keydown', onPictureCloseKeydown);

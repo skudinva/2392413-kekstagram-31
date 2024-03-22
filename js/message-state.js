45cee3e6ef3e4dc9abb237692b93439ff1a2b7a5
Skudin Vladimir <skudinva@gmail.com>
2024-03-22 18:47:41 +0700

    getPreventEvents излишняя функция, т.к. у тебя данные только внутри модуля используются, “геттеры” как правило добавляют чтобы данные наружу безопасно отдавать, но если добавил, то это не критично

--- js/message-state.js
+++ js/message-state.js
@@ -2,22 +2,18 @@ const messageState = {
   preventEvents: [],
 };
 
-const getPreventEvents = function () {
-  return messageState.preventEvents;
-};
-
 const setPreventEvents = function (value) {
   messageState.preventEvents = value;
 };
 
 const resetPreventEvents = function () {
-  getPreventEvents().forEach((evt) => {
+  messageState.preventEvents.forEach((evt) => {
     document.removeEventListener(evt.type, evt.cb);
   });
 };
 
 const recoverPreventEvents = function () {
-  getPreventEvents().forEach((evt) => {
+  messageState.preventEvents.forEach((evt) => {
     document.addEventListener(evt.type, evt.cb);
   });
 };
